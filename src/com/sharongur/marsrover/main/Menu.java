8fc252da2860a6c79cbdee27fd3946305abd3747
sharongur <gur.sharon82@gmail.com>
2016-12-11 14:50:55 +0200

    add file t previous commit

--- src/com/sharongur/marsrover/main/Menu.java
+++ src/com/sharongur/marsrover/main/Menu.java
@@ -1,16 +1,43 @@
 package com.sharongur.marsrover.main;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import com.sharongur.marsrover.handler.Handler;
 import com.sharongur.marsrover.model.Render;
+import com.sharongur.marsrover.model.Spawner;
+import com.sharongur.marsrover.model.State;
 
 public class Menu extends MouseAdapter implements Render {
+	
+	private Game game;
+	private Handler handler;
+	private Spawner spawner;
+	
+	private Font menuHeader = new Font("ariel", 1 , 50);
+	private Font menuBoxes = new Font("ariel", 1 , 30);
+	
+	public Menu(Game game, Handler handler, Spawner spawner) {
+		this.game = game;
+		this.handler = handler;
+		this.spawner = spawner;
+	}
 
 	public void mousePressed(MouseEvent e){
-		
+		int mx = e.getX();
+		int my = e.getY();
+		if(game.gameState == State.Menu){
+			// start game
+			if(mouseOver(mx, my, 210, 150, 200 ,64)){
+				game.gameState = State.Game;
+			}
+			if(mouseOver(mx, my, 210, 250, 200, 64)){
+				System.exit(1);
+			}
+		}
 	}
 	
 	public void mouseReleased(MouseEvent e){
@@ -23,8 +50,34 @@ public class Menu extends MouseAdapter implements Render {
 
 	@Override
 	public void render(Graphics g) {
-		g.setColor(Color.white);
-		g.drawRect(((int)Game.WIDTH / 2), (int)Game.HEIGHT-10, 100, 64);
+		if(game.gameState == State.Menu){
+			
+			g.setFont(menuHeader);
+			g.setColor(Color.white);
+			g.drawString("Menu", 240, 70);
+	
+			g.setFont(menuBoxes);
+			g.drawRect(210, 150, 200, 64);
+			g.drawString("Play", 270, 190);
+	
+			g.setFont(menuBoxes);
+			g.drawRect(210, 250, 200, 64);
+			g.drawString("Quit", 270, 290);
+	
+		}else{
+			
+		}
 	}
 	
+	private boolean mouseOver(int mx, int my, int x, int y, int width, int height){
+		if(mx> x && mx < x +width){
+			if (my > y && my < y + height){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
 }
--- src/com/sharongur/marsrover/main/Window.java
+++ src/com/sharongur/marsrover/main/Window.java
@@ -1,6 +1,7 @@
 package com.sharongur.marsrover.main;
 
 import java.awt.Canvas;
+import java.awt.Container;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
@@ -15,16 +16,20 @@ public class Window extends Canvas {
 	
 	public Window ( float width, float height, String title, Game game){
 		JFrame frame = new JFrame(title);
-		frame.getContentPane().setPreferredSize(new Dimension((int)width,(int)height));
-		frame.setMaximumSize(new Dimension((int)width,(int)height));
-		frame.setMinimumSize(new Dimension((int)width,(int)height));
+	
+		Container c = frame.getContentPane();
+		c.setBackground(game.gameBackgroundColor);
+		c.setPreferredSize(new Dimension((int)width, (int)height));
+		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		frame.setUndecorated(true);
 		frame.add(game);
+		frame.pack();
+		
 		frame.setVisible(true);
+		
 		game.start();
 		
 	}
